73dc801b54b64e957ff75dfffdc8f7a759f73dd9
ZeroKong413 <asdfzxcv5187@gmail.com>
2024-07-25 22:00:15 +0900

    0725

--- findme/urls.py
+++ findme/urls.py
@@ -3,7 +3,7 @@ from findme.views.base_views import recommend_content,recommend_content2, save_c
 
 urlpatterns = [
     path('', recommend_content, name = 'recommend_content'),
-    path('Content', recommend_content2, name = 'recommend_content2'),
+    path('Content/', recommend_content2, name = 'recommend_content2'),
     path('Save/', save_content, name = 'save_content'),
     path('Get_Saved/<str:nickname>/', get_saved_content, name = 'get_saved_content'),
     path('Delete/', delete_saved_content, name = 'delete_saved_content')
